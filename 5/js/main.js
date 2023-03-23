027a66a3d3c98946e428cf56424ca3faf7a51af6
Keks <keks@htmlacademy.ru>
2023-03-23 13:19:07 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,6 @@
+import {createDescriptionsOfPhotos} from './data.js';
+import { renderThumbnails } from './miniature.js';
+
+// console.log(createDescriptionsOfPhotos());
+
+renderThumbnails(createDescriptionsOfPhotos());
