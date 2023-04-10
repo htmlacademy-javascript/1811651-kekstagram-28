caba44f83cad7d604bb11d38211a4fab938c5e54
Igor <trololol-lolo@mail.ru>
2023-04-10 14:54:03 +1000

    Загружает данные с сервера и исправляет ошибки рендера комментариев

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-import {createDescriptionsOfPhotos} from './data.js';
+// import {createDescriptionsOfPhotos} from './data.js';
 import { renderGallery } from './gallery.js';
 import './form.js';
 import './scale.js';
 import './effect.js';
 
-renderGallery(createDescriptionsOfPhotos());
+// renderGallery(createDescriptionsOfPhotos());
+
+fetch('https://28.javascript.pages.academy/kekstagram/data')
+  .then((response) => response.json())
+  .then((data) => {
+    renderGallery(data);
+  });
