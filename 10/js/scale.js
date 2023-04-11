7013965b9a12712fe03e7aaba9a5f753820fd706
Keks <keks@htmlacademy.ru>
2023-04-11 12:51:20 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/scale.js
@@ -0,0 +1,41 @@
+const DEFAULT_SCALE = 100;
+const STEP_SCALE = 25;
+const MIN_SCALE = 25;
+const MAX_SCALE = 100;
+
+const smallerButtonScale = document.querySelector('.scale__control--smaller');
+const biggerButtonScale = document.querySelector('.scale__control--bigger');
+const scaleInputControl = document.querySelector('.scale__control--value');
+const imageScalePreview = document.querySelector('.img-upload__preview img');
+
+const scaleImage = (value) => {
+  imageScalePreview.style.transform = `scale(${value / 100})`;
+  scaleInputControl.value = `${value}%`;
+};
+
+const onSmallerButtonClick = () => {
+  const currentValue = parseInt(scaleInputControl.value, 10);
+  let newValue = currentValue - STEP_SCALE;
+  if (newValue < MIN_SCALE) {
+    newValue = MIN_SCALE;
+  }
+  scaleImage(newValue);
+};
+
+const onBiggerButtonClick = () => {
+  const currentValue = parseInt(scaleInputControl.value, 10);
+  let newValue = currentValue + STEP_SCALE;
+  if (newValue > MAX_SCALE) {
+    newValue = MAX_SCALE;
+  }
+  scaleImage(newValue);
+};
+
+const resetScale = () => {
+  imageScalePreview.style.transform = DEFAULT_SCALE;
+};
+
+smallerButtonScale.addEventListener('click', onSmallerButtonClick);
+biggerButtonScale.addEventListener('click', onBiggerButtonClick);
+
+export {resetScale};
---
+++ 10/js/util.js
@@ -0,0 +1,61 @@
+const ALERT_SHOW_TIME = 5000;
+
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const createRandomIdFromRangeGenerator = (min, max) => {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      //console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+const getRandomArray = (element, numberOfElement) => {
+  const idOfElement = createRandomIdFromRangeGenerator (0, element.length - 1);
+  const newElements = [];
+  for(let i = 0; i < numberOfElement; i++) {
+    newElements.push(element[idOfElement()]);
+  }
+  return newElements.join(' ');
+};
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+
+export {getRandomInteger, createRandomIdFromRangeGenerator, getRandomArray, isEscapeKey, showAlert};
