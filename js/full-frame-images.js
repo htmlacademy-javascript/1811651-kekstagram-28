3514f14119db0ed0ef562ab51f062d2e308130ec
Igor <trololol-lolo@mail.ru>
2023-04-13 23:04:53 +1000

    Небольшие исправления по защите

--- js/full-frame-images.js
+++ js/full-frame-images.js
@@ -3,11 +3,12 @@ const bigPicture = document.querySelector('.big-picture');
 const socialComments = bigPicture.querySelector('.social__comments');
 const userComment = socialComments.querySelector('.social__comment');
 const socialCommentCount = bigPicture.querySelector('.social__comment-count');
-const COMMENTS_PER_PORTION = 5;
+const COMMENTS_PER_PORTION = 4;
 const commentsLoader = bigPicture.querySelector('.comments-loader');
 const body = document.querySelector('body');
 const closeButton = document.querySelector('.big-picture__cancel');
 let shownComments = 0;
+let commentList;
 
 const createNewComment = (comment) => {
   const newComment = userComment.cloneNode(true);
@@ -17,38 +18,42 @@ const createNewComment = (comment) => {
   return newComment;
 };
 
+const renderComments = (comments) => {
+  shownComments += COMMENTS_PER_PORTION;
+  const fragment = document.createDocumentFragment();
+  if (shownComments >= comments.length) {
+    shownComments = comments.length;
+    commentsLoader.classList.add('hidden');
+    commentsLoader.removeEventListener('click', addComments);
+  } else {
+    commentsLoader.classList.remove('hidden');
+    commentsLoader.addEventListener('click', addComments);
+  }
+  comments.forEach((currentValue, index) => {
+    if (index < shownComments) {
+      fragment.append(createNewComment(comments[index]));
+    }
+  });
+  socialComments.innerHTML = '';
+  socialComments.appendChild(fragment);
+  socialCommentCount.innerHTML = `${shownComments} из <span class="comments-count">125</span> комментариев`;
+  bigPicture.querySelector('.comments-count').textContent = comments.length;
+};
+function addComments () {
+  renderComments(commentList);
+}
+
 const showBigPicture = (thumbnailId) => {
   socialComments.innerHTML = '';
+  shownComments = 0;
   const {comments, url, likes, description} = thumbnailId;
   bigPicture.querySelector('.social__caption').textContent = description;
   bigPicture.querySelector('.big-picture__img img').src = url;
   bigPicture.querySelector('.likes-count').textContent = likes;
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
-
-  const renderComments = () => {
-    shownComments += COMMENTS_PER_PORTION;
-    const fragment = document.createDocumentFragment();
-    if (shownComments >= comments.length) {
-      shownComments = comments.length;
-      commentsLoader.classList.add('hidden');
-      commentsLoader.removeEventListener('click', addComments);
-    } else {
-      commentsLoader.classList.remove('hidden');
-      commentsLoader.addEventListener('click', addComments);
-    }
-    for (let i = 0; i < shownComments; i++) {
-      const commentElement = createNewComment(comments[i]);
-      fragment.append(commentElement);
-    }
-    socialComments.innerHTML = '';
-    socialComments.appendChild(fragment);
-    socialCommentCount.innerHTML = `${shownComments} из <span class="comments-count">${comments.length}</span> комментариев`;
-  };
-  renderComments();
-  function addComments () {
-    renderComments();
-  }
+  renderComments(comments);
+  commentList = comments;
 };
 
 closeButton.addEventListener('click', () => {
